69fc2492c65a8fddea32800d5a32729658d43ba3
Jaimin Parmar <jaimins100@gmail.com>
2024-06-08 12:16:22 +0530

    exercise 1.3 - courseinfo

--- part-1/courseinfo/src/Content.jsx
+++ part-1/courseinfo/src/Content.jsx
@@ -2,17 +2,14 @@ import Part from "./Part"
 
 const Content = ({
   part1,
-  exercises1,
   part2,
-  exercises2,
   part3,
-  exercises3,
 }) => {
   return (
     <div>
-      <Part part={part1} exercises={exercises1} />
-      <Part part={part2} exercises={exercises2} />
-      <Part part={part3} exercises={exercises3} />
+      <Part part={part1.name} exercises={part1.exercises} />
+      <Part part={part2.name} exercises={part2.exercises} />
+      <Part part={part3.name} exercises={part3.exercises} />
     </div>
   )
 }
